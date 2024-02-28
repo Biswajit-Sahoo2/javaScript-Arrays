126609ebaecf84eff4ab6e4a77e3c9dcdc0a6e96
Biswajit-Sahoo2 <biswajitnov11@gmail.com>
2024-02-28 18:21:32 +0530

    Intial commit

---
+++ isIncludes.js
@@ -0,0 +1,11 @@
+//Q- Check if an element is present in an array or not
+
+const arr = [23, 56, 25, 12, 86, 46, 20];
+
+if (arr.includes(56)) {
+    console.log('56 is present in the array');
+}
+else {
+    console.log('56 is not present in the array');
+}
+
